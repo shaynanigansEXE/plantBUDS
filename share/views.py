84b9095deb75c4429e26df3bcf699941cc275357
zoe <zmckenzie@mills.edu>
2020-03-13 15:54:42 -0700

    iteration2-initial-view

--- share/views.py
+++ share/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from share.models import PlantTip
 
-# Create your views here.
+# iteration2-initial-view
 def index(request):
+    # Testing request object inside a view function
+    print('***********Testing ************')
+    print('request.header: ', request.headers['host'])
+    print('request.header: ', request.method)
+    print('request:' , request)
+    print('*******************************')
     if request.method == "GET":
-        return HttpResponse("Hello World!")
+        return render(request, 'share/static/share/startbootstrap/index.html')
