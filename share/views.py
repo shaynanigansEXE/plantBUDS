dff2a2ed09d859f84e75c3f0039fbfda59412017
Santiago <santiagogcgl@gmail.com>
2020-03-14 00:30:54 -0700

    fixing some files and folders in iteration2-initial-view

--- share/views.py
+++ share/views.py
@@ -4,11 +4,11 @@ from share.models import PlantTip
 
 # iteration2-initial-view
 def index(request):
-    # Testing request object inside a view function
-    print('***********Testing ************')
-    print('request.header: ', request.headers['host'])
-    print('request.header: ', request.method)
-    print('request:' , request)
-    print('*******************************')
+    # GET method requesting data
     if request.method == "GET":
-        return render(request, 'share/base.html')
+        # HTTP response for the GET method renders a message
+        return HttpResponse("Hello World!")
+
+def index(request):
+    if request.method == "GET":
+        return render(request, 'share/index.html')  # new line
