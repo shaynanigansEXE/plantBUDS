64716c064471cc6ac0311a0ce5062db5a0ea256c
zoe <zmckenzie@mills.edu>
2020-03-23 17:10:09 -0700

    fixes

--- share/admin.py
+++ share/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import PlantTip
 
 admin.site.register(PlantTip)
+admin.site.register(PlantBuddy)
+from .models import PlantBuddy
+from .models import PlantTip
+
+
+
 # Register your models here.
