a10b1c97cf09602063bfc2d6ce84828030975839
Shay <syano@mills.edu>
2020-03-23 19:48:15 -0700

    Changed db default to reflect postgres URI

--- plantBUDS/settings.py
+++ plantBUDS/settings.py
@@ -87,7 +87,8 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config(default='https://git.heroku.com/plant-buds.git')
+# Attempted fix #3: Tried adding the URI to this line for access to our database
+DATABASES['default'] = dj_database_url.config(default='postgres://ilhddvxatbiprq:562f504dd03d4c9c71ffb65189327eccad8153da25fa1835576e669b0775b5c4@ec2-52-71-85-210.compute-1.amazonaws.com:5432/d4clr4p5v40nnq')
 # The content of default= '  '   is  found in Heroku app URI, starts with postgres://...
 # allow us to connect locally the DB is Heroku
 
