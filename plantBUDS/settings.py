20c81bff138a2dadd5052b18bc27b5675bcf384d
Santiago <xfeng@mills.edu>
2020-03-30 03:52:09 -0700

    fixing static_root

--- plantBUDS/settings.py
+++ plantBUDS/settings.py
@@ -132,12 +132,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-<<<<<<< HEAD
-TATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-=======
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
->>>>>>> log-in-out
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
