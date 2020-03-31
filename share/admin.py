62af74f9dc322ecdac9b0a34b51c3c02f3cb0c32
zoe <zmckenzie@mills.edu>
2020-03-31 10:49:21 -0700

    fix version control

--- share/admin.py
+++ share/admin.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from .models import PlantTip, PlantBuddy
 
 admin.site.register(PlantTip)
-<<<<<<< HEAD
 admin.site.register(PlantBuddy)
 from .models import PlantBuddy
 from .models import PlantTip
 
 
-=======
->>>>>>> heroku-deployment
 
 # Register your models here.
